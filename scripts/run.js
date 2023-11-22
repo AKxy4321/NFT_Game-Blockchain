b9f008a20999a6d85c4a13dfc713d96c3ce59ac0
Aditya <adityakushal23@gmail.com>
2023-11-23 01:36:28 +0530

    o

--- scripts/run.js
+++ scripts/run.js
@@ -5,30 +5,21 @@ async function main() {
   const game = await gameContract.deploy(
     ["Master Chief", "Mario", "Pikachu"], // Names
     [
-      "https://i.imgur.com/G5vFLxn.jpeg", // Images
-      "https://i.imgur.com/q9xxo4d.jpeg",
-      "https://i.imgur.com/WMB6g9u.png",
+      "https://wallpapercave.com/dwp2x/wp12212164.jpg", // Images
+      "https://wallpapercave.com/dwp2x/wp11915469.jpg",
+      "https://wallpapercave.com/dwp2x/wp7174330.jpg"
     ],
     [200, 300, 100],  // HP values
     [50, 25, 100],    // Attack damage values
-    'Dragon',
-    'https://i.imgur.com/n87W4ylb.jpg',
+    'Diablo',
+    'https://wallpapercave.com/uwp/uwp3520412.webp',
     10000,           //Boss Hp
     50               //Boss Damage
   );
   await game.waitForDeployment();
 
   console.log("Contract deployed to:", game.target);
-
-  let txn;
-  txn = await game.mintCharacterNFT(2);
-  await txn.wait();
-  
-  txn = await game.attackBoss();
-  await txn.wait();
-  
-  txn = await game.attackBoss();
-  await txn.wait();
+  console.log('Done');
 }
 
 main().catch((error) => {
